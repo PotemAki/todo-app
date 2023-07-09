7fa7efcdf9036dbf1ee5b94bd9b04d00ec691740
PotemAki <michalmatyka194@gmail.com>
2023-07-09 16:50:09 +0200

    prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { ArchiveComponent } from './archive/archive.component';
